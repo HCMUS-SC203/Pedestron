24b2f2ab63cb18a5d006b56751e2ac61940847ae
ldn694 <70439334+ldn694@users.noreply.github.com>
2024-01-19 21:26:07 +0700

    Fixed run_detector

--- findWrongCases.py
+++ findWrongCases.py
@@ -22,6 +22,7 @@ def parse_args():
     parser.add_argument('input_img_dir', type=str, help='the dir of input images')
     parser.add_argument('output_dir', type=str, help='the dir for result images')
     parser.add_argument('gt_path', type=str, help='gt path')
+    parser.add_argument('threshold_IoU', type=float, help='threshold of IoU')
     parser.add_argument(
         '--launcher',
         choices=['none', 'pytorch', 'slurm', 'mpi'],
@@ -75,15 +76,19 @@ def draw_gt_bboxes(gt_path, image_name, image_path, output_dir):
         os.makedirs(output_dir)
     cv2.imwrite(os.path.join(output_dir, image_name), image)
 
-def get_detector_bboxes(model, image_name, output_dir):
-    image = cv2.imread(image_name)
+def get_detector_bboxes(model, image_path, score_thr=0.3):
+    image = cv2.imread(image_path)
     print(type(image))
     results = inference_detector(model, image)
     if isinstance(results, tuple):
         bbox_result = results[0]
     else:
         bbox_result = results
-    bboxes = np.vstack(bbox_result)
+    bboxes_with_scores = np.vstack(bbox_result)
+    bboxes = []
+    for bbox_with_score in bboxes_with_scores:
+        if bbox_with_score[4] > score_thr:
+            bboxes.append(bbox_with_score[:4])
     return bboxes
 
 def show_vis_ratio_list(gt_path):
@@ -100,30 +105,146 @@ def create_base_dir(dest):
     if not os.path.exists(basedir):
         os.makedirs(basedir)
 
+def get_IoU(box1, box2):
+    x1, y1, w1, h1 = box1
+    x2, y2, w2, h2 = box2
+    x = max(x1, x2)
+    y = max(y1, y2)
+    w = min(x1+w1, x2+w2) - x
+    h = min(y1+h1, y2+h2) - y
+    if w <= 0 or h <= 0:
+        return 0
+    return w*h/(w1*h1+w2*h2-w*h)
+
 def run_detector_on_dataset():
     args = parse_args()
     input_dir = args.input_img_dir
     output_dir = args.output_dir
     gt_path = args.gt_path
+    threshold_IoU = args.threshold_IoU
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     print(input_dir)
     eval_imgs = glob.glob(os.path.join(input_dir, '*.png'))
-    print(eval_imgs)
 
     model = init_detector(
         args.config, args.checkpoint, device=torch.device('cuda:0'))
 
-    # prog_bar = mmcv.ProgressBar(len(eval_imgs))
-    # for im in eval_imgs:
-    #     detections = mock_detector(model, im, output_dir)
-    #     prog_bar.update()
-    print(get_gt_bboxes("datasets/CityPersons/val_gt.json", "frankfurt_000000_000576_leftImg8bit.png"))
-    print(get_detector_bboxes(model, "datasets/CityPersons/leftImg8bit_trainvaltest/leftImg8bit/val_all_in_folder/frankfurt/frankfurt_000000_000576_leftImg8bit.png", output_dir))
+    prog_bar = mmcv.ProgressBar(len(eval_imgs))
+    cnt = 0
+    for im in eval_imgs:
+        cnt += 1
+        print("------------------")
+        print(f"Processing ({cnt}/{len(eval_imgs)}) {im} :")
+        detection_bbox = get_detector_bboxes(model, im)
+        gt_bboxes = get_gt_bboxes(gt_path, os.path.basename(im), False)
+        gt_ignore_boxes = get_gt_bboxes(gt_path, os.path.basename(im), True)
+        print(f"Detected: {len(detection_bbox)}")
+        print(f"Ground truth: {len(gt_bboxes)}")
+        print(f"Ignore: {len(gt_ignore_boxes)}")
+        edge_list = []
+        for i in range(len(detection_bbox)):
+            for j in range(len(gt_bboxes)):
+                x1, y1, w1, h1 = detection_bbox[i]
+                x2, y2, w2, h2 = gt_bboxes[j]
+                edge_list.append((i, j, get_IoU(detection_bbox[i], gt_bboxes[j])))
+        used_detection = set()
+        used_detection_ignore = set()
+        used_gt = set()
+        used_gt_ignore = set()
+        used_edge_id = set()
+        edge_list.sort(key=lambda x: x[2], reverse=True)
+        for i in len(edge_list):
+            edge = edge_list[i]
+            u, v, IoU = edge
+            if u in used_detection or v in used_gt or IoU < threshold_IoU:
+                continue
+            used_detection.add(u)
+            used_gt.add(v)
+            used_edge_id.add(i)
+        is_wrong_case = False
+        for i in range(len(detection_bbox)):
+            if i not in used_detection:
+                is_match_ignore = False
+                ignore_edge_list = []
+                for j in range(len(gt_ignore_boxes)):
+                    if j not in used_gt_ignore and get_IoU(detection_bbox[i], gt_ignore_boxes[j]) >= threshold_IoU:
+                        ignore_edge_list.append((j, get_IoU(detection_bbox[i], gt_ignore_boxes[j])))
+                ignore_edge_list.sort(key=lambda x: x[1], reverse=True)
+                for j in range(len(ignore_edge_list)):
+                    v, IoU = ignore_edge_list[j]
+                    if v not in used_gt_ignore:
+                        used_gt_ignore.add(v)
+                        is_match_ignore = True
+                        break
+                if not is_match_ignore:
+                    is_wrong_case = True
+                else:
+                    used_detection_ignore.add(i)
+        for i in range(len(gt_bboxes)):
+            if i not in used_gt:
+                is_wrong_case = True
+                break
+        RED = (0, 0, 255)
+        GREEN = (0, 255, 0)
+        BLUE = (255, 0, 0)
+        YELLOW = (0, 255, 255)
+        if is_wrong_case:
+            print("Wrong case!")
+            image = cv2.imread(im)
+            # Draw the ground truth
+            for i in range(len(gt_bboxes)):
+                x, y, w, h = gt_bboxes[i]
+                if i not in used_gt: # False negative
+                    cv2.rectangle(image, (int(x), int(y)), (int(x+w), int(y+h)), YELLOW, 1)
+                else: # True positive
+                    cv2.rectangle(image, (int(x), int(y)), (int(x+w), int(y+h)), BLUE, 1)
+            # Draw the detection
+            for i in range(len(detection_bbox)):
+                x, y, w, h = detection_bbox[i]
+                if i in used_detection: # True positive
+                    cv2.rectangle(image, (int(x), int(y)), (int(x+w), int(y+h)), GREEN, 1)
+                elif i in used_detection_ignore: # Ignore
+                    pass
+                else: # False positive
+                    cv2.rectangle(image, (int(x), int(y)), (int(x+w), int(y+h)), RED, 1)
+            # Output the image
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+            cv2.imwrite(os.path.join(output_dir, os.path.basename(im)), image)
+            # Output log file
+            with open(os.path.join(output_dir, os.path.basename(im) + ".log"), "w") as f:
+                f.write(f"Detected: {len(detection_bbox)}\n")
+                for i in range(len(detection_bbox)):
+                    x, y, w, h = detection_bbox[i]
+                    f.write(f"{i} {x} {y} {w} {h}\n")
+                f.write("\n")
+                f.write(f"Ground truth: {len(gt_bboxes)}\n")
+                for i in range(len(gt_bboxes)):
+                    x, y, w, h = gt_bboxes[i]
+                    f.write(f"{i} {x} {y} {w} {h}\n")
+                f.write("\n")
+                f.write(f"Egde list: {len(edge_list)}\n")
+                for i in range(len(edge_list)):
+                    u, v, IoU = edge_list[i]
+                    f.write(f"{u} {v} {IoU}\n")
+                f.write("\n")
+                f.write(f"True positive: {len(used_edge_id)}\n")
+                for i in range(len(used_edge_id)):
+                    u, v, IoU = edge_list[used_edge_id[i]]
+                    f.write(f"{detection_bbox[u]} {gt_bboxes[v]} {IoU}\n")
+                f.write(f"False positive: {len(detection_bbox) - len(used_detection)}\n")
+                for i in range(len(detection_bbox)):
+                    if i not in used_detection:
+                        f.write(f"{i} {detection_bbox[i]}\n")
+                f.write(f"False negative: {len(gt_bboxes) - len(used_gt)}\n")
+                for i in range(len(gt_bboxes)):
+                    if i not in used_gt:
+                        f.write(f"{i} {gt_bboxes[i]}\n")
+                f.close()
+        else:
+            print("Correct case!")
+        prog_bar.update()
 
 if __name__ == '__main__':
-    # run_detector_on_dataset()
-    img_path = "datasets/CityPersons/leftImg8bit_trainvaltest/leftImg8bit/val_all_in_folder/frankfurt_000000_001236_leftImg8bit.png"
-    img_name = "frankfurt_000000_001236_leftImg8bit.png"
-    gt_path = "datasets/CityPersons/val_gt.json"
-    draw_gt_bboxes(gt_path, img_name, img_path, "test/")
+    run_detector_on_dataset()
